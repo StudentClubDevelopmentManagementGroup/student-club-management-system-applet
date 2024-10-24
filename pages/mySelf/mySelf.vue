ddc5e9ae1ec4de4b463b26c3f765d361d950e3bd
jw21811 <jw21811@126.com>
2024-10-24 16:56:25 +0800

    Feature:完成"个人页面"布局
    初步完成个人页面布局，正在开发的功能区在点按后会出现提示。
    
    我很怀疑我们是否有头像这个东西，以及学生如何获取自身所在的社团信息

--- pages/mySelf/mySelf.vue
+++ pages/mySelf/mySelf.vue
@@ -1,6 +1,41 @@
 <template>
-	<view>
-		myself
+	<view id="main-content" style="background-image: url('/static/images/背景.jpg');">
+		<view id="user-info">
+			<image id="avatar" src="../../static/logo.png"></image>
+			<view style="display: flex;flex-direction: column;">
+				<text id="user-name">{{userName}}</text>
+				<text id="user-department">XXXX社团</text>
+			</view>
+			<image @click="developing()" id="setting" src="../../static/svgs/mySelf_setting.svg"></image>
+		</view>
+		<view id="functionArea1">
+			<view class="item" @click="developing()">
+				<image src="../../static/svgs/mySelf_补卡进程.svg"></image>
+				<text>补卡进程</text>
+			</view>
+			<view class="item" @click="developing()">
+				<image src="../../static/svgs/mySelf_签到详情.svg"></image>
+				<text>签到详情</text>
+			</view>
+			<view class="item" @click="developing()">
+				<image src="../../static/svgs/mySelf_账号安全.svg"></image>
+				<text>账号安全</text>
+			</view>
+		</view>
+		<view id="functionArea2">
+			<view class="item" @click="developing()">
+				<image src="../../static/svgs/mySelf_help.svg"></image>
+				<text>帮助中心</text>
+			</view>
+			<view class="item" @click="developing()">
+				<image src="../../static/svgs/mySelf_issue.svg"></image>
+				<text>问题反馈</text>
+			</view>
+			<view class="item" @click="developing()">
+				<image src="../../static/svgs/mySelf_aboutUs.svg"></image>
+				<text>关于我们</text>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +43,29 @@
 	export default {
 		data() {
 			return {
-				
+				app: null,
+				userName: "",
+				userDepartment: "",
 			}
 		},
-		methods: {
+		onLoad() {
+			this.app = getApp()
 			
+			this.userName = this.app.globalData.userData.user_info.name
+		},
+
+		methods: {
+			developing() {
+				uni.showToast({
+					title: "该功能正在开发",
+					icon: "none"
+				})
+				return
+			}
 		}
 	}
 </script>
 
-<style>
-
+<style scoped>
+	@import url(./mySelf.css);
 </style>
