15e5ba3af2edb1560e90e0d24a51dadbbd5278e8
Git_xiaoyi <l1508214141@gmail.com>
2024-11-13 23:19:19 +0800

    查看打卡记录页面0.1

--- pages/attendance/makeUpCheckIn/makeUpCheckIn.vue
+++ pages/attendance/makeUpCheckIn/makeUpCheckIn.vue
@@ -1,22 +1,42 @@
 <template>
-	<view>
-		
-	</view>
+  <view>
+    <view v-if="clubInfo.length > 0">
+      <view v-for="(club, index) in clubInfo" :key="index" class="club-item">
+        <p>社团名称：{{ club.clubName }}</p>
+        <p>社团 ID：{{ club.clubId }}</p>
+        <p>部门名称：{{ club.departmentName }}</p>
+        <p>角色：{{ club.role }}</p>
+        <hr />
+      </view>
+    </view>
+    <view v-else>
+      <p>你尚未加入任何社团。</p>
+    </view>
+  </view>
 </template>
-
 <script>
 	export default {
 		data() {
 			return {
-				
+				clubInfo: [] // 存储社团信息
 			}
 		},
-		methods: {
-			
+		created() {
+			// 获取全局数据
+			const app = getApp();
+			this.clubInfo = app.globalData.userData?.clubInfo || [];
 		}
 	}
 </script>
+<style scoped>
+.club-item {
+  padding: 10px;
+  border: 1px solid #ddd;
+  margin-bottom: 10px;
+  border-radius: 5px;
+}
 
-<style>
-
+p {
+  margin: 5px 0;
+}
 </style>
